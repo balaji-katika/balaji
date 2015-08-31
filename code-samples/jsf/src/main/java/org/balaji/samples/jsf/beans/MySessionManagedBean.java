8734fa07ecdf91bc04e542e01e125b9f7c791303
Balaji Katika <bkatika@walmartlabs.com>
2015-08-31 21:08:07 +0530

    PrimeFace examples added

---
+++ code-samples/jsf/src/main/java/org/balaji/samples/jsf/beans/MySessionManagedBean.java
@@ -0,0 +1,30 @@
+package org.balaji.samples.jsf.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.annotation.PostConstruct;
+
+@SuppressWarnings("restriction")
+@ManagedBean(name="mySessionBean")
+@SessionScoped
+public class MySessionManagedBean {
+	@PostConstruct
+	public void init() {
+		
+	}
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void welcome() {
+		
+	}
+	
+}
