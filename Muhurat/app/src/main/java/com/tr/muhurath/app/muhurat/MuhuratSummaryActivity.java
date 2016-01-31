84cd0269ef8a5a0ca9dd151dde15c44643b2fe44
gkna <geddam.naidu@gmail.com>
2016-01-31 21:18:13 +0530

    add shared button

--- Muhurat/app/src/main/java/com/tr/muhurath/app/muhurat/MuhuratSummaryActivity.java
+++ Muhurat/app/src/main/java/com/tr/muhurath/app/muhurat/MuhuratSummaryActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.luckycatlabs.sunrisesunset.SunriseSunsetCalculator;
@@ -26,7 +29,10 @@ import java.util.TimeZone;
  * Created by Balaji Katika (balaji.katika@gmail.com) on 1/30/16.
  */
 public class MuhuratSummaryActivity extends AppCompatActivity {
+
     public static final String LBL_DATE_DISPLAY = "E dd-MMM-yyyy";
+
+    private StringBuffer summaryText = new StringBuffer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +66,7 @@ public class MuhuratSummaryActivity extends AppCompatActivity {
         SimpleDateFormat displayDateFormat = new SimpleDateFormat(LBL_DATE_DISPLAY);
         TextView dateHolder = (TextView) findViewById(R.id.txtMuhuratDateHolder);
         dateHolder.setText("Muhurat for " + displayDateFormat.format(selectedDate));
+        summaryText.append("Muhurat for " + displayDateFormat.format(selectedDate));
 
         Kaal kaal = null;
         TextView kalHolder = null;
@@ -68,19 +75,23 @@ public class MuhuratSummaryActivity extends AppCompatActivity {
         kaal = new GuliKaal();
         kalHolder = (TextView) findViewById(R.id.txtMuhuratGuli);
         kalHolder.setText("Gulika : " + kaal.getMuhuratForDisplay(sunRise, sunSet));
+        summaryText.append("Gulika : " + kaal.getMuhuratForDisplay(sunRise, sunSet));
 
         //Display Rahu Kaal
         kaal = new RahuKaal();
         kalHolder = (TextView) findViewById(R.id.txtMuhuratRahu);
         kalHolder.setText("Rahu Kaalam : " + kaal.getMuhuratForDisplay(sunRise, sunSet));
+        summaryText.append("Rahu Kaalam : " + kaal.getMuhuratForDisplay(sunRise, sunSet));
 
         //Display Yama Kaal
         kaal = new YamaGandaKaal();
         kalHolder = (TextView) findViewById(R.id.txtMuhuratYama);
         kalHolder.setText("YamaGandam : " + kaal.getMuhuratForDisplay(sunRise, sunSet));
+        summaryText.append("YamaGandam : " + kaal.getMuhuratForDisplay(sunRise, sunSet));
 
         kalHolder = (TextView) findViewById(R.id.txtMuhuraShoolam);
         kalHolder.setText("Shoolam : " + DateUtils.getShoolamDirection(selectedDate));
+        summaryText.append("Shoolam : " + DateUtils.getShoolamDirection(selectedDate));
 
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(selectedDate);
@@ -88,13 +99,27 @@ public class MuhuratSummaryActivity extends AppCompatActivity {
         //Display Sun Rise
         kalHolder = (TextView) findViewById(R.id.txtMuhuraSunRise);
         kalHolder.setText("Sun Rise : " + calculator.getOfficialSunriseForDate(calendar));
+        summaryText.append("Sun Rise : " + calculator.getOfficialSunriseForDate(calendar));
 
         //Display Sun Set
         kalHolder = (TextView) findViewById(R.id.txtMuhuraSunSet);
         kalHolder.setText("Sun Set : " + calculator.getOfficialSunsetForDate(calendar));
+        summaryText.append("Sun Set : " + calculator.getOfficialSunsetForDate(calendar));
 
         //Set back button on Tool bar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    private void showWatsApp() {
+        Button watsApp = (Button) findViewById(R.id.shareButton);
+        watsApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+                sharingIntent.setType("text/html");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, Html.fromHtml("<p>" + summaryText.toString() + "</p>"));
+                startActivity(Intent.createChooser(sharingIntent, "Share using"));
+            }
+        });
+    }
 }
