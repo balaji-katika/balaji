06fcf9093190c6b09cddd8bd387a5f71a4360874
Balaji Katika <bkatika@walmartlabs.com>
2016-01-31 11:21:10 +0530

    Added summary activity

--- Muhurat/app/src/main/java/com/tr/muhurath/app/muhurat/Muhurat.java
+++ Muhurat/app/src/main/java/com/tr/muhurath/app/muhurat/Muhurat.java
@@ -1,31 +1,46 @@
 package com.tr.muhurath.app.muhurat;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.widget.Button;
+import android.content.Context;
 
 public class Muhurat extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_muhurat);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        addListenterOnCalcButton();
+    }
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+    /**
+     * Adds the listener for 'Calculate' Button
+     */
+    public void addListenterOnCalcButton() {
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.btnCalc);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                Intent intent = new Intent(context, MuhuratSummaryActivity.class);
+                startActivity(intent);
+
             }
-        });*/
+
+        });
+
     }
 
     @Override
