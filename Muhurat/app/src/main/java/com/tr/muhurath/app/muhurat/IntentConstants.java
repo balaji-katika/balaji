a72e183b60ddb1113b66a8a7e417120449715a17
Balaji Katika <bkatika@walmartlabs.com>
2016-02-02 19:37:28 +0530

    Removed Muhurat

--- Muhurat/app/src/main/java/com/tr/muhurath/app/muhurat/IntentConstants.java
+++
@@ -1,9 +0,0 @@
-package com.tr.muhurath.app.muhurat;
-
-/**
- * Holder for constants related to Intent
- * Created by Balaji Katika (balaji.katika@gmail.com) on 1/30/16.
- */
-public interface IntentConstants {
-    String DATE_DDMMYYYY = "com.tr.muhurath.app.muhurat.selected_date";
-}
